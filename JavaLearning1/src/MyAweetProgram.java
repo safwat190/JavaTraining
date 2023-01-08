dd7fc8f9a8f630f4453212143fd85ae4f1c319d1
Ahmed Safwat <a.safwat.a190x@gmail.com>
2023-01-08 11:01:06 -0500

    another commit

--- JavaLearning1/src/MyAweetProgram.java
+++ JavaLearning1/src/MyAweetProgram.java
@@ -1,16 +1,22 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class MyAweetProgram {
-
+	
+	Collection<String> stuff;
+	
 	public static void main(String[] args) throws FileNotFoundException {
 		
 		Scanner in = new Scanner(new File("Files/test.txt"));
 		
-		List<String> names = new ArrayList<String>();
+		LinkedList<String> names = new LinkedList<String>();
 		
 		while(in.hasNextLine()) {
 			names.add(in.nextLine());
@@ -19,18 +25,38 @@ public class MyAweetProgram {
 		for(int i =0; i < names.size();i++) {
 			System.out.println(names.get(i));
 		}
-		/*Scanner in = new Scanner(System.in);
 		
-		int size = in.nextInt();
-		int[] grades = new int[size];
+		System.out.println("\n"+"Iterators");
+		Iterator<String> it = names.iterator();
 		
-		for(int i =0; i < size; i++) {
-			grades[i] = in.nextInt();
+		while(it.hasNext()) {
+			System.out.println(it.next());
 		}
-		in.close();
 		
-		for(int i =0; i < size; i++) {
-			System.out.println(grades[i]);
-		}*/
+		System.out.println("\n"+"For each");
+		for(String s: names) {
+			System.out.println(s);
+		}
+		
+		System.out.println("\n"+"List Iterators");
+		ListIterator<String> listit = names.listIterator();
+		listit.next();
+		listit.next();
+		listit.add("Susan");
+		for(String s: names) {
+			System.out.println(s);
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
